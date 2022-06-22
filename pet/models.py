894276743860adb2af342d1de6a9d20a6f1d6c93
Moringa School <wangu.mwangi@student.moringaschool.com>
2022-06-22 12:27:47 +0300

    first commit

--- pet/models.py
+++ pet/models.py
@@ -1,3 +1,38 @@
 from django.db import models
 
 # Create your models here.
+from django.db import models
+from datetime import datetime
+from django.contrib.auth.models import User
+
+# Create your models here.
+OUR_ROLES =(
+    ("Dr","doctor"),
+    ("Po","pet_owner")
+)
+    
+
+
+class Profle(models.Model):
+    name = models.CharField(max_length=255)
+    email = models.EmailField(max_length=255)
+    image = models.ImageField(upload_to='images')
+    bio = models.CharField(max_length=100000)
+    phone = models.IntegerField()
+    roles = models.CharField(max_length=255,choices=OUR_ROLES)
+    
+    
+    
+class Pet(models.Model):
+    age = models.IntegerField()
+    name = models.CharField(max_length=255)
+    user = models.ForeignKey(User, blank=True, null=True, on_delete=models.CASCADE)
+    
+class Appointment(models.Model):
+
+    user = models.ForeignKey(User, blank=True, null=True, on_delete=models.CASCADE)
+    age = models.ForeignKey('Pet', on_delete=models.CASCADE)
+    date = models.DateTimeField(auto_now_add=True)
+    
+
+    
