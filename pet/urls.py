93650250f99165e1ac2c0a9271200322936e0995
davine94 <davine94>
2022-06-22 14:35:58 +0300

    Added logic on the forms

--- pet/urls.py
+++ pet/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    
+    path('/contact', contactView, name='contact'),
+    path('/success', successView, name='success'),
     
 ]
