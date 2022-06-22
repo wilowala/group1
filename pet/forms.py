451befe8f780d824a4e8525870fc7804410e5f59
davine94 <davine94>
2022-06-22 15:55:57 +0300

    Created a model class and connected it to forms

--- pet/forms.py
+++ pet/forms.py
@@ -1,8 +1,7 @@
-from django import forms
+from django.forms import ModelForm
+from .models import Contact
 
-class ContactForm(forms.Form):
-    first_name = forms.CharField(max_length=50)
-    last_name = forms.CharField(max_length=50)
-    email_address = forms.EmailField(max_length=150)
-    message = forms.CharField(widget=forms.Textarea, max_length= 2000)
-   
+class ContactForm(ModelForm):
+    class Meta:
+        model = Contact
+        fields ='__all__'
