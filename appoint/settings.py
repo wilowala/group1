26130db78f3ab2244ccc46fa48e019b974435f1d
William Owalla <william.owalla@student.moringaschool.com>
2022-06-23 13:24:54 +0300

    register bootstrap

--- appoint/settings.py
+++ appoint/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'pet',
     'bootstrap5',
     'appointments',
+    'crispy_forms',
 ]
 
 MIDDLEWARE = [
@@ -120,7 +121,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static')
+]
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 STATIC_URL = 'static/'
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
